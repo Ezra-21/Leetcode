f855393f209b6cada0f7f5133ffcf821533eddba
Ezra <168126853+Ezra-21@users.noreply.github.com>
2024-10-24 17:02:53 +0300

    Time: 35 ms (67.18%), Space: 16.5 MB (23.87%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -2,7 +2,7 @@ class Solution:
     def fib(self, n: int) -> int:
         f = [0,1]
         for i in range(2,n+1):
-            f[i] = self.fib(n-1)+self.fib(n-2)
+            f.append( f[i-1]+f[i-2])
         
         return f[n]
 
