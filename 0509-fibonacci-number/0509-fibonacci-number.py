7fc7a6047ae53f23b31cf6fdbc1144d26e3661bc
Ezra <168126853+Ezra-21@users.noreply.github.com>
2024-10-24 17:05:15 +0300

    Time: 28 ms (93.98%), Space: 16.6 MB (23.87%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,6 +1,12 @@
 class Solution:
     def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+
         prev1,prev2 = 0,1
+        
         for i in range(2,n+1):
             temp = prev1+prev2
             prev1 = prev2
