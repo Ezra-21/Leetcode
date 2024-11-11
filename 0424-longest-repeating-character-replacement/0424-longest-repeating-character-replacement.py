1644140ece626e6049f8f1374a876df2a85eb524
Ezra <168126853+Ezra-21@users.noreply.github.com>
2024-11-10 23:06:57 -0800

    Time: 248 ms (8.03%), Space: 16.8 MB (21.92%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -0,0 +1,24 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        result = 0
+        for val in set(s):
+            result = max(result,self.solve(s,k,val))
+        return result
+
+
+    def solve(self,s,k,target):
+        ans,j = 0,0
+        for i in range(len(s)):
+            if s[i] != target:
+                k-=1
+                while k < 0:
+                    if s[j] != target:
+                        k+=1
+                    j+=1
+            ans = max(ans,i-j+1)
+                
+
+        return ans
+
+
+        
