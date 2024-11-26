1f2f64da4df11d6f86ecced253ae4cbd167fa264
Ezra <168126853+Ezra-21@users.noreply.github.com>
2024-11-26 13:39:33 -0800

    Time: 0 ms (100%), Space: 17.4 MB (5.61%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,61 +1,21 @@
+from collections import deque
+
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        class Node:
-            def __init__(self,value):
-                self.value = value
-                self.next = None
-        class stack:
-            def __init__(self):
-                self.head = None
-                self.size = 0
-
-            def isempty(self):
-                return self.size == 0
-
-            def push(self,value):
-                if self.isempty():
-                    self.head = Node(value)
-                else:
-                    node = Node(value)
-                    node.next = self.head
-                    self.head = node
-                self.size += 1
-
-            def pop(self):
-                if self.isempty():
-                    return None
-                else:
-                    remove = self.head
-                    self.head = remove.next
-                    remove.next = None
-                    self.size -= 1
-                    return remove.value
-        stack = stack()
-        alpha = []
-        dot = ''
-        for val in path:
-            if val != '/':
-                alpha.append(val)
+        stack = deque()
+        components = path.split('/')  # Split the path by '/' into components
+        
+        for component in components:
+            if component == '' or component == '.':
+                # Ignore empty components or '.' (current directory)
+                continue
+            elif component == '..':
+                # '..' means go up one directory, so pop from stack if it's not empty
+                if stack:
+                    stack.pop()
             else:
-                if alpha:
-                    res = ''.join(alpha)
-                    if res == '..':  # Handle '..'
-                        stack.pop()
-                    elif res[0]!='.' or len(res) >= 2:
-                        stack.push(res)
-                    alpha = []
-                
-        if alpha:
-            res = ''.join(alpha)
-            if res == '..':  # Handle '..'
-                stack.pop()
-            elif res[0]!='.' or len(res) >= 2:
-                    stack.push(res)
-
-        res = ''   
-        while not stack.isempty():
-            value = stack.pop()
-            res = '/' + value + res
-        return res if res else '/'
-
-                
+                # Add valid directory names to the stack
+                stack.append(component)
+        
+        # Join the stack into the simplified path
+        return '/' + '/'.join(stack)
