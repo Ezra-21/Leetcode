1912ee099cf7a6b5c58689ed23d3a84d6b6de1c7
Ezra <168126853+Ezra-21@users.noreply.github.com>
2024-11-26 22:47:23 -0800

    Time: 2 ms (45.54%), Space: 17.1 MB (5.61%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,61 +1,17 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        class Node:
-            def __init__(self,value):
-                self.value = value
-                self.next = None
-        class stack:
-            def __init__(self):
-                self.head = None
-                self.size = 0
-
-            def isempty(self):
-                return self.size == 0
-
-            def push(self,value):
-                if self.isempty():
-                    self.head = Node(value)
-                else:
-                    node = Node(value)
-                    node.next = self.head
-                    self.head = node
-                self.size += 1
-
-            def pop(self):
-                if self.isempty():
-                    return None
-                else:
-                    remove = self.head
-                    self.head = remove.next
-                    remove.next = None
-                    self.size -= 1
-                    return remove.value
-        stack = stack()
-        alpha = []
-        dot = ''
-        for val in path:
-            if val != '/':
-                alpha.append(val)
+        path = path.split('/')
+        stack = deque()
+        for directory in path:
+            if directory == '.' or directory == '':
+                continue
+            elif directory == '..':
+                if stack:
+                    stack.pop()
             else:
-                if alpha:
-                    res = ''.join(alpha)
-                    if res == '..':  # Handle '..'
-                        stack.pop()
-                    elif res[0]!='.' or len(res) >= 2:
-                        stack.push(res)
-                    alpha = []
-                
-        if alpha:
-            res = ''.join(alpha)
-            if res == '..':  # Handle '..'
-                stack.pop()
-            elif res[0]!='.' or len(res) >= 2:
-                    stack.push(res)
+                stack.append(directory)
 
-        res = ''   
-        while not stack.isempty():
-            value = stack.pop()
-            res = '/' + value + res
-        return res if res else '/'
+        return '/' if not stack else ('/'+'/'.join(stack))
 
-                
+
+        
