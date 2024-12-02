d62459a62a3a9856f0252dab22e00572e7480042
Ezra <168126853+Ezra-21@users.noreply.github.com>
2024-12-02 11:27:45 -0800

    Time: 40 ms (97.68%), Space: 22.5 MB (54.93%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -1,26 +1,25 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
         
-        store = []
-        index = 0
-        count_odd = start = ans = 0
+        
+        count_odd =  ans = 0
+        valid_subarray = 0
 
         j = 0
         for i in range(len(nums)):
             if nums[i] %2 ==1:
-                store.append(i)
                 count_odd += 1
+                valid_subarray = 0
             
-            while count_odd > k:
+            while count_odd == k:
+                valid_subarray += 1
+
                 if nums[j] %2 ==1:
-                    index += 1
                     count_odd -= 1
+
+                j+=1
                 
-                j += 1
-                start = j
-            
-            if count_odd == k:
-                ans += (store[index]-start+1)
+            ans += valid_subarray
 
         return ans
 
