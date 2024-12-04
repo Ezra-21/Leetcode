338fc99dba2a3867b0e022b88a022f3af6f6dd29
Ezra <168126853+Ezra-21@users.noreply.github.com>
2024-12-04 10:31:39 -0800

    Time: 207 ms (84.32%), Space: 17.3 MB (9.14%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,29 +1,25 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        nums.sort()
-        n = len(nums)
-        ans = []
-        for i in range(n-3):
-            if i>0 and nums[i] == nums[i-1]:
-                continue
-            for j in range(i+1,n-2):
-                if j>i+1 and nums[j] == nums[j-1]:
-                    continue
-                l,r = j+1,n-1
-                while l<r:
-                    summ = nums[i]+nums[j]+nums[l]+nums[r]
+        i = 0
+        answer =[]
+        nums = sorted(nums)
+        while i<len(nums)-3:
+            while i>0 and i<len(nums)-3 and nums[i] == nums[i-1]: i += 1
+            j = i + 1
+            k = len(nums)-2
+            while j<k:
+                while j>i+1 and j<len(nums)-1 and nums[j] == nums[j-1]: j += 1
+                k = j + 1
+                l = len(nums)-1
+                while k < l:
+                    value = nums[i] + nums[j] + nums[k] + nums[l]
+                    if value == target:
+                        answer.append([nums[i],nums[j],nums[k],nums[l]])
+                        k += 1
+                        while k<l and nums[k] == nums[k-1]: k += 1
+                    elif value>target: l -= 1
+                    else: k += 1
+                j += 1
 
-                    if summ < target:
-                        l+=1
-                    elif summ > target:
-                        r-=1
-                    else:
-                        ans.append([nums[i],nums[j],nums[l],nums[r]])
-                        while l<r and nums[l]== nums[l+1]:
-                            l+=1
-                        while l<r and nums[r]== nums[r-1]:
-                            r-=1
-                        
-                        l+=1
-                        r-=1
-        return ans
+            i += 1
+        return answer
