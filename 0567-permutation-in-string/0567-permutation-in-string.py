79e208d1c87936b17159f6e059b17f2283947e60
Ezra <168126853+Ezra-21@users.noreply.github.com>
2024-12-07 04:48:24 -0800

    Time: 67 ms (27.95%), Space: 17.3 MB (10%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,31 +1,26 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         n,m = len(s1),len(s2)
-        if n>m: return False
-        dic_s1 = {}
-        dic_s2 = {}
+        hashh_s1 = Counter()
+        hashh_s2 = Counter()
 
         for val in s1:
-            if val not in dic_s1:
-                dic_s1[val] = 0
-            dic_s1[val] += 1
+            hashh_s1[val]+=1
 
-        for i in range(n):
-            if s2[i] not in dic_s2:
-                dic_s2[s2[i]] = 0
-            dic_s2[s2[i]] += 1
-        
-        for i in range(n,m):
-            if dic_s1 == dic_s2:
-                return True
+        j = 0
+        for i,val in enumerate(s2):
+            hashh_s2[val]+=1
 
-            dic_s2[s2[i-n]] -= 1
-            if dic_s2[s2[i-n]] == 0:
-                del dic_s2[s2[i-n]]
-            
-            if s2[i] not in dic_s2:
-                dic_s2[s2[i]] = 0
-            dic_s2[s2[i]] += 1
-        
-        
-        return dic_s1 == dic_s2
+            if (i-j+1) > n:
+                hashh_s2[s2[j]]-=1
+                if hashh_s2[s2[j]] == 0:
+                    del hashh_s2[s2[j]]
+                j+=1
+                
+
+            if (i-j+1) == n:
+                if hashh_s1 == hashh_s2:
+                    return True
+
+        return False
+
