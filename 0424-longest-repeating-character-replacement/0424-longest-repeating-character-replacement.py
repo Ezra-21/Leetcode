5b9e3b83edee33f595c4d965a863b1f0ea9d786f
Ezra <168126853+Ezra-21@users.noreply.github.com>
2024-12-07 05:11:54 -0800

    Time: 331 ms (5.77%), Space: 17.6 MB (10.3%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,26 +1,26 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        letter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
- 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-        res = 0
-        for val in letter:
-            res = max(res,self.solve(s,k,val))
-        return res
+        result = 0
+        for val in set(s):
+            result = max(result,self.solve(s,k,val))
+        return result
+
 
     def solve(self,s,k,target):
-        j = 0
-        ans = 0
-        count_non = 0
+        ans,j = 0,0
         for i in range(len(s)):
-            if s[i]!=target:
-                count_non+=1
-
-            while count_non>k:
-                if s[j]!=target:
-                    count_non-=1
+            if s[i] != target:
+                k-=1
+            while k<0:
+                if s[j] != target:
+                    k+=1
                 j+=1
 
-
+                    
             ans = max(ans,i-j+1)
+                
+
         return ans
 
+
+        
