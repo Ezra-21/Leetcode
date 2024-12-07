ebb96ade62256fadb9f492d0266d19f4fec21fdd
Ezra <168126853+Ezra-21@users.noreply.github.com>
2024-12-07 08:16:01 -0800

    Time: 3 ms (24.79%), Space: 17.6 MB (5.08%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,14 +1,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = deque()
-        hashh = {')':'(',']':'[','}':'{'}
-
+        stack = []
+        bracket_map = { '}':'{',']':'[',')':'(' }
         for bracket in s:
-            if bracket in hashh:
-                if not stack or stack.pop()!=hashh[bracket]:
-                    return False
-            else:
+            if bracket in bracket_map.values():
                 stack.append(bracket)
+            else:
+                if not stack or stack.pop() != bracket_map[bracket]:
+                    return False
+        return not stack
 
-        return len(stack) == 0
+            
         
