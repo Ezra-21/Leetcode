15b9f8be8c66e11f9b98c7e08fe66fe557aafcdb
Ezra <168126853+Ezra-21@users.noreply.github.com>
2024-12-07 03:26:47 -0800

    Time: 35 ms (96.13%), Space: 20.3 MB (24.03%) - LeetHub

--- 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.py
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.py
@@ -1,16 +1,20 @@
 class Solution:
     def maxTurbulenceSize(self, arr: List[int]) -> int:
-        def solve(arr,evod):
-            ans = 1
-            j = 0
-            for i in range(len(arr)-1):
-                if i%2 == evod:
-                    if arr[i] <= arr[i+1]:
-                        j=i+1
-                else:
-                    if arr[i] >= arr[i+1]:
-                        j=i+1
-                ans = max(ans,(i-j+2))
-            return ans
-          
-        return max(solve(arr,0),solve(arr,1))
+        l = 0
+        ans,prv = 1,''
+
+        for r in range(1,len(arr)):
+            if arr[r-1] > arr[r] and prv != '>':
+                ans = max(ans,(r-l+1))
+                prv = '>'
+            elif arr[r-1] < arr[r] and prv != '<':
+                ans = max(ans,(r-l+1))
+                prv = '<'
+            elif arr[r-1] == arr[r]:
+                l = r
+                prev = '='
+            else:
+                l = r-1
+
+        return ans
+
