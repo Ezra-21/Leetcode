fab93edd4b58a6f1faba1d4d6a7d574e554fdd1c
Ezra <168126853+Ezra-21@users.noreply.github.com>
2024-12-08 02:54:21 -0800

    Time: 291 ms (34.8%), Space: 17.8 MB (39.98%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
@@ -3,26 +3,18 @@ class Solution:
         def operation(num):
             count = 0
             while num != 1:
-                count+=1
-                if num%2==1:
-                    num = 3*num+1
+                count += 1
+                if num % 2 == 1:
+                    num = 3 * num + 1
                 else:
                     num //= 2
             return count
 
-        arr = []
+        # Create a list of tuples (power, number)
+        arr = [(operation(i), i) for i in range(lo, hi + 1)]
 
-        for i in range(lo,hi+1):
-            arr.append([operation(i),i])
+        # Sort by power first, and by the number itself in case of ties
+        arr.sort(key=lambda x: (x[0], x[1]))
 
-        
-        arr.sort()
-
-        
-        return arr[k-1][1]
-
-
-
-            
-            
-        
+        # Return the k-th smallest number
+        return arr[k - 1][1]
