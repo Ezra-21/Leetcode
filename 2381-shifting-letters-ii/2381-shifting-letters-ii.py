2ed79cdcbba40041636895ffbe1cd12489c924a0
Ezra <168126853+Ezra-21@users.noreply.github.com>
2024-12-08 01:32:12 -0800

    Time: 63 ms (52.97%), Space: 41.3 MB (26.35%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.py
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.py
@@ -3,19 +3,18 @@ class Solution:
         n = len(s)
         arr = [0]*(n+1)
 
-        for left,right,shift in shifts:
-            if shift == 1:
-                arr[left] += 1
-                arr[right+1] -= 1
-            else:
-                arr[left] -= 1
-                arr[right+1] +=1 
+        for start,end,inc in shifts:
+            rr = 1 if inc==1 else -1
+            arr[start] += rr
+            arr[end+1] -= rr
 
-        for i in range(1,n):
+        for i in range(1,n+1):
             arr[i] += arr[i-1]
-        ans = []
-        for i in range(n):
-            val = chr((ord(s[i])-ord('a') + arr[i])%26+ord('a'))
-            ans.append(val)
 
-        return ''.join(ans)
+        ans = []
+
+        for i in range(n):
+            char = chr(((ord(s[i])-ord('a'))+(arr[i]%26))%26+ord('a'))
+            ans.append(char)
+
+        return "".join(ans)
