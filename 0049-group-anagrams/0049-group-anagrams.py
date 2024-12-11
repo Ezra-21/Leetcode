278bdb6c4f98a1bbdc98405602eca97c00fc8e76
Ezra <168126853+Ezra-21@users.noreply.github.com>
2024-12-11 04:59:50 -0800

    Time: 14 ms (51.29%), Space: 20.4 MB (44.43%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashh = defaultdict(list)
+        for char in strs:
+            key = ''.join(sorted(char))
+            hashh[key].append(char)
+
+        ans = []
+
+        for value in hashh.values():
+            ans.append(value)
+
+        return ans
+
+        
