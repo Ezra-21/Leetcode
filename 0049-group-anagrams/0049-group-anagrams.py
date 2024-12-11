24599210b0700b0913bb46c9b5e67eea7acce43e
Ezra <168126853+Ezra-21@users.noreply.github.com>
2024-12-11 05:04:52 -0800

    Time: 15 ms (49.35%), Space: 22.3 MB (7.18%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,8 +2,10 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         hashh = defaultdict(list)
         for char in strs:
-            key = tuple(sorted(char))
-            hashh[key].append(char)
+            count = [0]*26
+            for c in char:
+                count[ord(c)-ord('a')]+=1
+            hashh[tuple(count)].append(char)
 
         
 
