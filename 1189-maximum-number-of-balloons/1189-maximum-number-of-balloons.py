1c3d38741850babccc8b904e1cb0816077bc6f49
Ezra <168126853+Ezra-21@users.noreply.github.com>
2024-12-12 02:48:39 -0800

    Time: 4 ms (23.01%), Space: 17.6 MB (6.46%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,16 +1,8 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
         hashh = Counter(text)
-        string = "balloon"
-        ans = float('inf')
-        for char in string:
-            if char not in hashh:
-                return 0
-            if char=='l' or char=='o':
-                ans = min(ans,hashh[char]//2)
-            else:
-                ans = min(ans,hashh[char])
-
-        return ans
-
-        
+        return min(hashh.get('b', 0),
+                   hashh.get('a', 0),
+                   hashh.get('l', 0) // 2,
+                   hashh.get('o', 0) // 2,
+                   hashh.get('n', 0))
