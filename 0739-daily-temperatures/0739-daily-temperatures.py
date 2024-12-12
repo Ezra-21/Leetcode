79a55d2a37247d849e15316e847473bdfe4d6376
Ezra <168126853+Ezra-21@users.noreply.github.com>
2024-12-11 23:45:36 -0800

    Time: 155 ms (10.43%), Space: 34.5 MB (5.53%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -5,9 +5,6 @@ class Solution:
         ans = [0]*n
 
         for i,temp in enumerate(temperatures):
-            if i==0:
-                stack.append([temp,i])
-                continue
             while stack and stack[-1][0] < temp:
                 ans[stack[-1][1]] = i-stack[-1][1]
                 stack.pop()
