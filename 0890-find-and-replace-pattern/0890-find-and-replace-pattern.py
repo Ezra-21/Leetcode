4051e0c888b5ba9c0e88acecf64ec9b386b5771a
Ezra <168126853+Ezra-21@users.noreply.github.com>
2024-12-13 23:01:31 -0800

    Time: 3 ms (25.23%), Space: 17.3 MB (21.95%) - LeetHub

---
+++ 0890-find-and-replace-pattern/0890-find-and-replace-pattern.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
+        ans = []
+        for word in words:
+            look_first = {}
+            look_second = {}
+            flag = True
+            for i in range(len(word)):
+                if word[i] not in look_first and pattern[i] not in look_second:
+                    look_first[word[i]] = pattern[i]
+                    look_second[pattern[i]] = word[i]
+                    continue
+
+                if word[i] in look_first:
+                    if look_first[word[i]] != pattern[i]:
+                        flag = False
+
+                if pattern[i] in look_second:
+                    if look_second[pattern[i]] != word[i]:
+                        flag = False
+
+            if flag:
+                ans.append(word)
+
+        return ans
+                
+
+
+        
