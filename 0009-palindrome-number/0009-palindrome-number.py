f7b7fad2a422c81720ae272563d914b4e458556e
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-01-29 21:24:21 -0800

    Time: 15 ms (19.54%), Space: 17.8 MB (30.1%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -8,7 +8,7 @@ class Solution:
         length = math.floor(math.log10(x))+1
         place_value = 10**(length-1)
 
-        while place_value!=1:
+        while place_value>1:
             if (x//place_value)!=(x%10):
                 return False
             x %= place_value
