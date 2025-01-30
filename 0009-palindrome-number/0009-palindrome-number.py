bf9f3611d8c76a7e5adfa9120655fe239f4ebd15
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-01-29 21:24:49 -0800

    Time: 14 ms (21.85%), Space: 18 MB (15.03%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,20 +1,25 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x<0:
+        if x < 0:
             return False
         if x == 0:
             return True
 
         length = math.floor(math.log10(x))+1
-        place_value = 10**(length-1)
+        if length == 1:
+            return True
+        place = 10**(length-1)
 
-        while place_value>1:
-            if (x//place_value)!=(x%10):
+        while place>1:
+            if (x//place) != (x%10):
                 return False
-            x %= place_value
-            x //= 10 
+            
+            x %= place
+            x //= 10
 
-            place_value //= 100
+            place //=100
+
         return True
 
 
+        
