b8c69667cee1ec68d4b81de47e4af7a0cebcee5e
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-02-02 13:43:00 -0800

    Time: 0 ms (100%), Space: 18 MB (6.74%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,10 +1,14 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        t = [0,1,1]
-        if n<3:
-            return t[n]
-        
-        for i in range(3,n+1):
-            t[0],t[1],t[2] = t[1],t[2],sum(t)
-        return t[-1]
+        def solve(n,hashh):
+            if n==1 or n==2:
+                return 1
+            if n==0:
+                return 0
+            if n not in hashh:
+                hashh[n] = solve(n-1,hashh)+solve(n-2,hashh)+solve(n-3,hashh)
+            return hashh[n]
+        hashh = {}
+        return solve(n,hashh)
+
         
