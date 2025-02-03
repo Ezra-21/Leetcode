ea1c0387612e833f3fd5cccdc2624ca1cf23b267
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-02-03 03:23:38 -0800

    Time: 7 ms (90.19%), Space: 18 MB (43.86%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,8 +1,8 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s)!=len(t):
-            return False
-        hashh1 = Counter(s)
-        hashh2 = Counter(t)
-        return hashh1==hashh2
+        hashh_s = Counter(s)
+        hashh_t = Counter(t)
+
+        return hashh_s == hashh_t
+
         
