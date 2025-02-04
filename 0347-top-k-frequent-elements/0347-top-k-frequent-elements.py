d35c2bcbdc1364fc45ce6095c74a7a3a849452c5
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-02-03 23:37:11 -0800

    Time: 7 ms (53.84%), Space: 21.3 MB (55.55%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,22 +1,18 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        hashh = {}
-        for val in nums:
-            if val not in hashh:
-                hashh[val] = 0
-            hashh[val] += 1
-
-        arr = []
-        for num,fre in hashh.items():
-            arr.append([fre,num])
-        
-        arr.sort(reverse=True)
-
+        hashh = Counter(nums)
         ans = []
+        mostFrequent_element = []
+        for num,fre in hashh.items():
+            mostFrequent_element.append([fre,num])
+
+        mostFrequent_element.sort(reverse=True)
         for i in range(k):
-            ans.append(arr[i][1])
+            ans.append(mostFrequent_element[i][1])
 
         return ans
 
 
+
+        
         
