5fe68379af735c39978078f2fa53f60bb4d45368
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-02-06 07:44:14 -0800

    Time: 3 ms (71.94%), Space: 18.6 MB (17.47%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,23 +3,25 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        def make_colmn(i,j,matrix):
-            for j in range(len(matrix[0])):
-                if matrix[i][j]!=0:
-                    matrix[i][j] = ''
-        def make_row(i,j,matrix):
-            for i in range(len(matrix)):
-                if matrix[i][j]!=0:
-                    matrix[i][j] = ''
+
+        col = [0]*len(matrix[0])
+        row = [0]*len(matrix)
 
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if matrix[i][j] == 0:
-                    make_colmn(i,j,matrix)
-                    make_row(i,j,matrix)
-        
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if matrix[i][j]=='':
-                    matrix[i][j] = 0
+                    col[j] = 1
+                    row[i] = 1
 
+        for i in range(len(col)):
+            if col[i] == 1:
+                for j in range(len(matrix)):
+                    matrix[j][i] = 0
+
+        for i in range(len(row)):
+            if row[i] == 1:
+                for j in range(len(matrix[0])):
+                    matrix[i][j] = 0
+        return matrix
+            
+        
