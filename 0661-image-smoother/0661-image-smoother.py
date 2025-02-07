ba6af7567753f87ee0b2aa6f48d0650118289236
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-02-06 21:22:05 -0800

    Time: 156 ms (66.89%), Space: 18.6 MB (66.31%) - LeetHub

--- 0661-image-smoother/0661-image-smoother.py
+++ 0661-image-smoother/0661-image-smoother.py
@@ -1,17 +1,18 @@
 class Solution:
     def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
-        row = len(img)
-        col = len(img[0])
-        new = [[0]*col for _ in range(row)]
+        row,col = len(img),len(img[0])
+        smooth_img =[[0]*col for _ in range(row)] 
+
         for i in range(row):
             for j in range(col):
-                summ,count = 0,0
-                
+                summ = 0
+                num = 0
                 for r in range(i-1,i+2):
                     for c in range(j-1,j+2):
-                        if (r>=0 and c>=0 and r<=row-1 and c<=col-1):
-                            count+=1
+                        if r>=0 and r<row and c>=0 and c<col:
                             summ+=img[r][c]
-                
-                new[i][j] = summ//count
-        return new
+                            num+=1
+                smooth_img[i][j] = summ//num
+
+        return smooth_img
+        
