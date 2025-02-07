844f5d722e0d66ffa91d8ba6f697a5f70a844fd3
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-02-06 22:24:11 -0800

    Time: 13 ms (57.46%), Space: 20.8 MB (55.11%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,12 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         hashh = defaultdict(list)
-        for char in strs:
-            key = tuple(sorted(char))
-            hashh[key].append(char)
 
-        
+        for word in strs:
+            common = ''.join(sorted(word))
+            hashh[common].append(word)
+        ans = []
+       
+        for value in hashh.values():
+            ans.append(value)
 
-        return list(hashh.values())
-
+        return ans
         
