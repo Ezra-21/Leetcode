e6ecd03f735b27d70d7d189e784908223ce0917e
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-02-07 03:04:47 -0800

    Time: 750 ms (10.33%), Space: 79.2 MB (9.39%) - LeetHub

---
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.py
@@ -0,0 +1,16 @@
+class Solution: 
+    def tupleSameProduct(self, nums: List[int]) -> int:
+        hashh = Counter()
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                pro = nums[i]*nums[j]
+                hashh[pro]+=1
+
+        ans = 0
+        print(hashh)
+        for value in hashh.values():
+
+            ans+=4*((value*(value-1)))    
+
+        return ans         
+        
