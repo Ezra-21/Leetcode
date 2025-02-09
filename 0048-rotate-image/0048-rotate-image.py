be33b586697e3c47f01f1fda67c136a1db2ce2d8
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-02-09 09:38:16 -0800

    Time: 4 ms (2.42%), Space: 17.9 MB (20.92%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,17 +3,15 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        left,right = 0,len(matrix)-1
-        while left<right:
-            top,bottom = left,right
-            for i in range(right-left):
-                temp = matrix[top][left+i]
-                matrix[top][left+i] = matrix[bottom-i][left]
-                matrix[bottom-i][left] = matrix[bottom][right-i]
-                matrix[bottom][right-i] = matrix[top+i][right]
-                matrix[top+i][right] = temp
-            left += 1
-            right -= 1
+        n = len(matrix)
+        new_mat = [[0]*n for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                new_mat[j][i] = matrix[i][j]
 
+        for row in new_mat:
+            row.reverse()
 
-        
+        for i in range(n):
+            for j in range(n):
+                matrix[i][j] = new_mat[i][j]
