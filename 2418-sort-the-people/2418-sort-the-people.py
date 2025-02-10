c4c018179b134b103b703803228aa853cef283a2
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-02-09 22:53:19 -0800

    Time: 773 ms (5.03%), Space: 18.4 MB (24.31%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,13 +1,20 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-
+        n = len(names)
         hashh = {}
-        for i in range(len(names)):
-            hashh[heights[i]]= names[i]
+        for height,name in zip(heights,names):
+            hashh[height] = name
 
-        heights.sort(reverse=True)
-
-        for i in range(len(names)):
-            names[i] = hashh[heights[i]]
-
+        for i in range(len(heights)):
+            for j in range(len(heights)-1):
+                if heights[j]<heights[j+1]:
+                    heights[j],heights[j+1] = heights[j+1],heights[j]
+                    
+        for i,height in enumerate(heights):
+            names[i] = hashh[height]
         return names
+
+
+
+
+        
