c2373e6496a5ce34feca020a9fae58dc8435909e
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-02-10 18:21:48 +0300

    Time: 87 ms (88.07%), Space: 24.1 MB (85.82%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -4,15 +4,15 @@ class Solution:
             return nums
         
         min_val, max_val = min(nums), max(nums)
-        count = [0] * (max_val - min_val + 1)
+        count = [0] * (max_val+abs(min_val) + 1)
         
         for num in nums:
-            count[num - min_val] += 1
+            count[num+abs(min_val)] += 1
         
         index = 0
         for i in range(len(count)):
             while count[i] > 0:
-                nums[index] = i + min_val
+                nums[index] = i - abs(min_val)
                 index += 1
                 count[i] -= 1
         
