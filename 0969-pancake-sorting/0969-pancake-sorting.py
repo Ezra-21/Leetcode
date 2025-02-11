11032de55660eff7fb964eae65236ff61d1661da
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-02-11 11:49:26 +0300

    Time: 0 ms (100%), Space: 17.8 MB (36.55%) - LeetHub

--- 0969-pancake-sorting/0969-pancake-sorting.py
+++ 0969-pancake-sorting/0969-pancake-sorting.py
@@ -1,26 +1,23 @@
 class Solution:
     def pancakeSort(self, arr: List[int]) -> List[int]:
-        Max_element = len(arr)
-        n = len(arr)
+        maximum = len(arr)
         ans = []
-
-        for i in range(n):
-            index = arr.index(Max_element)
-
-            if index == 0:
-                arr[:n-i] = arr[:n-i][::-1]
-                ans.append(n-i)
-            elif 0 < index < n-1:
-                arr[:index+1] = arr[:index+1][::-1]
-                ans.append(index+1)
-                arr[:n-i] = arr[:n-i][::-1]
-                ans.append(n-i)
-               
-            Max_element -= 1
+        for i in range(len(arr)):
+            maxi = maximum-i
+            idx = arr.index(maxi)
+            if idx == 0:
+                arr = arr[:maxi][::-1]+arr[maxi:]
+                ans.append(maxi)
+            elif idx == maxi-1:
+                continue
+            else:
+                arr = arr[:idx+1][::-1]+arr[idx+1:]
+                ans.append(idx+1)
+                arr = arr[:maxi][::-1]+arr[maxi:]
+                ans.append(maxi)
 
         return ans
-               
-            
+
 
 
         
