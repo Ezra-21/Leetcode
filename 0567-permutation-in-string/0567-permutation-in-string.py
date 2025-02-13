851f0e2671aa9546f1879b07b9b37a6a08f1dcc5
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-02-13 18:27:38 +0300

    Time: 289 ms (20.11%), Space: 18 MB (38.14%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,26 +1,18 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         n,m = len(s1),len(s2)
-        hashh_s1 = Counter()
-        hashh_s2 = Counter()
+        hashh_s1 = Counter(s1)
+        hashh_per = Counter(s2[:n-1])
+        l = 0
+        for i in range(n-1,m):
+            hashh_per[s2[i]]+=1
+            print(hashh_per)
+            if hashh_s1 == hashh_per:
+                return True
 
-        for val in s1:
-            hashh_s1[val]+=1
-
-        j = 0
-        for i,val in enumerate(s2):
-            hashh_s2[val]+=1
-
-            if (i-j+1) > n:
-                hashh_s2[s2[j]]-=1
-                if hashh_s2[s2[j]] == 0:
-                    del hashh_s2[s2[j]]
-                j+=1
-                
-
-            if (i-j+1) == n:
-                if hashh_s1 == hashh_s2:
-                    return True
+            hashh_per[s2[l]]-=1
+            if hashh_per[s2[l]] == 0:
+                del hashh_per[s2[l]]
+            l+=1
 
         return False
-
