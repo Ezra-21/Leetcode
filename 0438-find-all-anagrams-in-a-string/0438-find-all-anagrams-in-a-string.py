c32ab1b59fc5ee3f71de16885784c506f988470b
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-02-13 11:44:03 +0300

    Time: 236 ms (22.26%), Space: 18.3 MB (55.31%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,36 +1,21 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        len_p,len_s = len(p),len(s)
+        n,m = len(s),len(p)
+        hashh_p = Counter(p)
+        hashh_anag = Counter(s[:m-1])
         ans = []
-        if len_p > len_s:
-            return ans
-        dic_s = {}
-        dic_p = {}
-        
-        for val in p:
-            if val not in dic_p:
-                dic_p[val] = 0
-            dic_p[val]+=1
+        l = 0
+        for i in range(m-1,n):
+            hashh_anag[s[i]]+=1
 
-        for i in range(len_p):
-            if s[i] not in dic_s:
-                dic_s[s[i]] = 0
-            dic_s[s[i]]+=1
-        
-        for i in range(len_p,len_s):
-            if dic_s == dic_p:
-                ans.append(i-len_p)
-            dic_s[s[i-len_p]] -= 1
-            if dic_s[s[i-len_p]] == 0:
-                del dic_s[s[i-len_p]]
+            if hashh_p == hashh_anag:
+                ans.append(l)
             
-            if s[i] not in dic_s:
-                dic_s[s[i]] = 0
-            dic_s[s[i]]+=1
-        if dic_s == dic_p:
-            ans.append(len_s-len_p)
+            hashh_anag[s[l]]-=1
+            if hashh_anag[s[l]] == 0:
+                del hashh_anag[s[l]]
+            l+=1
 
         return ans
             
-        
         
