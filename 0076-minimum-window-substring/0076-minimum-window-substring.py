90944093bcb3b4f1aa09a8c88211cd59ecd3fbc5
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-02-14 10:44:42 +0300

    Time: 465 ms (12.94%), Space: 18.1 MB (90.8%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,21 +1,26 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        def check(hashh1,hashh2):
+            for val,fre in hashh2.items():
+                if hashh1[val]<fre:
+                    return False
+
+            return True
+
         n,m = len(s),len(t)
         if m>n:
             return ''
         hashh_t = Counter(t)
         hashh_include = Counter()
         length,index = float('inf'),[]
-        check = 0
         j = 0
         for i in range(n):
             hashh_include[s[i]]+=1
-            while all(hashh_include[val]>=fre for val,fre in hashh_t.items()):
+            while check(hashh_include,hashh_t):
                 if length>(i-j+1):
                     length = (i-j+1)
                     index = [j,i]
-                if s[j] in hashh_t:
-                    hashh_include[s[j]]-=1
+                hashh_include[s[j]]-=1
                 j+=1
 
         return s[index[0]:index[1]+1] if index else ''
