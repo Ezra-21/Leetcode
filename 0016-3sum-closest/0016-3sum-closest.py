c88f9551e19f42096363e887040d4b4b02c46a90
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-02-14 04:16:44 +0300

    Time: 358 ms (47.73%), Space: 17.7 MB (92.64%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -3,16 +3,19 @@ class Solution:
         nums.sort()
         ans = float('inf')
         for i in range(len(nums)):
-            left, right = i + 1, len(nums) - 1
-            while left < right:
-                summ = nums[i] + nums[left] + nums[right]
-                if abs(target - summ) < abs(target - ans):
+            left,right = i+1,len(nums)-1
+            while left<right:
+                summ = nums[i]+nums[left]+nums[right]
+                if abs(target-summ)<abs(target-ans):
                     ans = summ
 
-                if summ > target:
-                    right -= 1
-                elif summ < target:
-                    left += 1
+                if summ>target:
+                    right-=1
+                elif summ<target:
+                    left+=1
                 else:
                     return summ
         return ans
+
+
+        
