ac65de51ff74bccd67d2c729db6bae7196e61f10
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-02-18 16:21:09 +0300

    Time: 40 ms (45.94%), Space: 31.4 MB (91.19%) - LeetHub

--- 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.py
+++ 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.py
@@ -1,13 +1,23 @@
 class ProductOfNumbers:
-
     def __init__(self):
-        self.arr = []
-        
-    def add(self, num: int) -> None:
-        self.arr.append(num)
+        self.temp = 1
+        self.prefix = []
 
+    def add(self, num: int) -> None:
+        if num==0:
+            self.temp = 1
+            self.prefix = []
+        else:
+            self.prefix.append(self.temp*num)
+            self.temp = self.prefix[-1]
+            
     def getProduct(self, k: int) -> int:
-        return prod(self.arr[-k:])
+        if k>len(self.prefix):
+            return 0
+        if k==len(self.prefix):
+            return self.prefix[-1]
+
+        return self.prefix[-1]//self.prefix[-k-1]
         
 
 
@@ -15,3 +25,41 @@ class ProductOfNumbers:
 # obj = ProductOfNumbers()
 # obj.add(num)
 # param_2 = obj.getProduct(k)
+
+
+
+
+
+'''
+
+class ProductOfNumbers:
+    def __init__(self):
+        self.prefix = [1]  # Prefix product (1 as identity)
+        self.last_zero = -1  # Track last zero index
+
+    def add(self, num: int) -> None:
+        if num == 0:
+            self.prefix = [1]  # Reset prefix product
+            self.last_zero = len(self.prefix) - 1  # Track last zero position
+        else:
+            self.prefix.append(self.prefix[-1] * num)  # Multiply last value
+
+    def getProduct(self, k: int) -> int:
+        if k >= len(self.prefix):  # If `k` exceeds available values, return 0
+            return 0
+        return self.prefix[-1] // self.prefix[-k-1]  # Compute product efficiently
+
+
+# Example usage:
+obj = ProductOfNumbers()
+obj.add(3)
+obj.add(0)
+obj.add(2)
+obj.add(5)
+obj.add(4)
+print(obj.getProduct(2))  # Output: 20 (5 * 4)
+print(obj.getProduct(3))  # Output: 0 (Contains 0)
+print(obj.getProduct(4))  # Output: 0 (Contains 0)
+obj.add(8)
+print(obj.getProduct(2))  # Output: 32 (4 * 8)
+'''
