f104bdb0a1c77e4540f631a5d678f25209050b25
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-02-27 15:15:51 +0300

    Time: 157 ms (48.26%), Space: 22.9 MB (69.1%) - LeetHub

---
+++ 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.py
@@ -0,0 +1,32 @@
+class Solution:
+    def waysToMakeFair(self, nums: List[int]) -> int:
+        summ_even = 0
+        summ_odd = 0
+        for i in range(len(nums)):
+            if i%2==0:
+                summ_even+=nums[i]
+            else:
+                summ_odd+=nums[i]
+        print(summ_even,summ_odd)
+        prev_odd = 0
+        prev_even = 0
+        ans = 0
+
+        for i in range(len(nums)):
+            temp_odd,temp_even = summ_odd,summ_even
+            if i%2==0:
+                temp_even-=nums[i]
+            else:
+                temp_odd-=nums[i]
+
+            new_even = temp_odd-prev_odd+prev_even
+            new_odd = temp_even-prev_even+prev_odd
+
+            if new_even == new_odd:
+                ans+=1
+
+            if i%2==0:
+                prev_even+=nums[i]
+            else:
+                prev_odd+=nums[i]
+        return ans
