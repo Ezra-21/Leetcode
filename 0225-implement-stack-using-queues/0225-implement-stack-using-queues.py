bd9bf2536c7fc433d3d3fdc43dd4e69dc850579d
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-03-02 08:54:11 +0300

    Time: 0 ms (100%), Space: 18 MB (23.71%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -2,23 +2,22 @@ class MyStack:
 
     def __init__(self):
         self.stack = deque()
-
-        
+        self.size = 0
 
     def push(self, x: int) -> None:
         self.stack.append(x)
-        
+        self.size+=1
 
     def pop(self) -> int:
-        return self.stack.pop()
-        
+        num = self.stack.pop()
+        self.size-=1
+        return num
 
     def top(self) -> int:
         return self.stack[-1]
-        
 
     def empty(self) -> bool:
-        return len(self.stack)==0
+        return self.size==0
         
 
 
