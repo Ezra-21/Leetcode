4d79b9cfa71db9c59d7af964843391e2172ea614
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-03-04 17:48:38 +0300

    Time: 139 ms (8.37%), Space: 19 MB (18.33%) - LeetHub

--- 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.py
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.py
@@ -1,9 +1,9 @@
 class Solution:
     def checkPowersOfThree(self, n: int) -> bool:
         arr = []
-        m = int(math.pow(n,1/3))+1
+        m = int(math.pow(n,1/2))+1
         for i in range(m):
-            b=math.pow(3,i)
+            b = 3**i
             arr.append(b)
         for i in range(len(arr)-1,-1,-1):
             if n>=arr[i]:
