49427a359e347dfa03200611a7457b5d49d4969f
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-03-06 19:14:32 +0300

    Time: 447 ms (32.85%), Space: 39.7 MB (16.22%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,27 +1,27 @@
+
 class Solution:
-    def longestSubarray(self, num: List[int], limit: int) -> int:
-
-        Max_queue = deque()
-        Min_queue = deque()
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+        min_heap = []  # Stores (value, index) -> Get min value
+        max_heap = []  # Stores (-value, index) -> Get max value (since Python has only min-heap)
+        j = 0  # Left pointer
         ans = 0
-        j = 0
-        for i in range(len(num)):
-            while Max_queue and num[Max_queue[-1]]<num[i]:
-                Max_queue.pop()
-            Max_queue.append(i)
+        
+        for i in range(len(nums)):
+            heappush(min_heap, (nums[i], i))  # Push current value in min-heap
+            heappush(max_heap, (-nums[i], i))  # Push negative value in max-heap (for max tracking)
 
-            while Min_queue and num[Min_queue[-1]]>num[i]:
-                Min_queue.pop()
-            Min_queue.append(i)
+            # Check if the difference between max and min exceeds `limit`
+            while -max_heap[0][0] - min_heap[0][0] > limit:
+                # Move left pointer to remove invalid elements
+                j += 1
 
-            while abs(num[Max_queue[0]] - num[Min_queue[0]])> limit:
-                if num[Min_queue[0]] == num[j]:
-                    Min_queue.popleft()
-                if num[Max_queue[0]] == num[j]:
-                    Max_queue.popleft()
-                j+=1
+                # Remove out-of-window elements
+                while min_heap and min_heap[0][1] < j:
+                    heappop(min_heap)
+                while max_heap and max_heap[0][1] < j:
+                    heappop(max_heap)
 
-            ans = max(ans,i-j+1)
+            # Update the maximum valid window size
+            ans = max(ans, i - j + 1)
 
         return ans
-        
