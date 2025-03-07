3cba500e06ad82831e9776a0f6b7a8ed383e3e45
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-03-07 09:13:04 +0300

    Time: 3566 ms (16.25%), Space: 28.2 MB (60.94%) - LeetHub

---
+++ 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.py
@@ -0,0 +1,27 @@
+class Solution:
+    def closestPrimes(self, left: int, right: int) -> List[int]:
+        arr = [0]*(10**6+1)
+        arr[1] = -1
+        for i in range(2,right+1):
+            if arr[i]==0:
+                j = i*i
+                while j<right+1:
+                    arr[j] = -1
+                    j+=i
+        
+        collect = []
+        for num in range(left,right+1):
+            if arr[num]==0:
+                collect.append(num)
+
+        Minn = float('inf')
+        hashh = {}
+        for i in range(len(collect)-1):
+            diff = collect[i+1]-collect[i]
+            if Minn>diff:
+                Minn = diff
+                hashh[Minn] = [collect[i],collect[i+1]]
+
+        return hashh[Minn] if Minn!=float('inf') else [-1,-1]
+
+        
