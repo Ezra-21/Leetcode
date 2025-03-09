df7e5151adede34fa38e4ff5f0b2c8866601f95f
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-03-09 22:46:43 +0300

    Time: 0 ms (100%), Space: 18.1 MB (21.85%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,16 +1,13 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        hashh = {val:i for i,val in enumerate(nums1)}
-        ans = [-1]*(len(nums1))
+        n = len(nums1)
+        dic = {val:i for i,val in enumerate(nums1)}
+        ans = [-1]*n
         stack = []
         for val in nums2:
             while stack and stack[-1]<val:
-                num = stack.pop()
-                idx = hashh[num]
+                idx = dic[stack.pop()]
                 ans[idx] = val
-            if val in hashh:
+            if val in dic:
                 stack.append(val)
-
         return ans
-
-
