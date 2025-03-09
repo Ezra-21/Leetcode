ef100924fc4e49eeefadd1a183d0f9b98c8720e4
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-03-09 21:18:26 +0300

    Time: 783 ms (19.6%), Space: 22 MB (24.25%) - LeetHub

--- 3208-alternating-groups-ii/3208-alternating-groups-ii.py
+++ 3208-alternating-groups-ii/3208-alternating-groups-ii.py
@@ -1,16 +1,15 @@
 class Solution:
     def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
+        colors.extend(colors[:k-1])
+        n = len(colors)
         count = 0
-
-        colors.extend(colors[:k-1]) # if k = 3 we add 2 numbers like the above question
-
-        L = 0
-        for R in range(1, len(colors)):
-            
-            if colors[R] == colors[R-1]:
-                L = R
-            if R - L + 1 == k:
-                count += 1
-                L += 1
+        stack = deque()
+        for i in range(len(colors)):
+            if stack and stack[-1] == colors[i%n]:
+                stack.clear()
+            stack.append(colors[i%n])
+            if len(stack)==k:
+                stack.popleft()
+                count+=1
 
         return count
