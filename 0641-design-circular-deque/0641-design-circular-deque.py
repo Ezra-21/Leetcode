042c03d72334dffa9c03470a81883c8094bb2362
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-03-10 00:30:58 +0300

    Time: 8 ms (50.42%), Space: 18.6 MB (54.64%) - LeetHub

--- 0641-design-circular-deque/0641-design-circular-deque.py
+++ 0641-design-circular-deque/0641-design-circular-deque.py
@@ -1,53 +1,52 @@
 class MyCircularDeque:
 
     def __init__(self, k: int):
-        self.queue = deque(maxlen = k)
-        self.k = k
-        
-
+        self.queue = deque()
+        self.size = k
+        self.count = 0
     def insertFront(self, value: int) -> bool:
-        if self.isFull():
+        if self.count>=self.size:
             return False
         self.queue.appendleft(value)
+        self.count+=1
         return True
-        
 
     def insertLast(self, value: int) -> bool:
-        if self.isFull():
+        if self.count>=self.size:
             return False
         self.queue.append(value)
+        self.count+=1
         return True
 
     def deleteFront(self) -> bool:
-        if self.isEmpty():
+        if self.count<=0:
             return False
         self.queue.popleft()
+        self.count-=1
         return True
 
     def deleteLast(self) -> bool:
-        if self.isEmpty():
+        if self.count<=0:
             return False
         self.queue.pop()
+        self.count-=1
         return True
 
     def getFront(self) -> int:
-        if self.isEmpty():
-            return -1 
+        if self.count<=0:
+            return -1
         return self.queue[0]
-        
 
     def getRear(self) -> int:
-        if self.isEmpty():
+        if self.count<=0:
             return -1
         return self.queue[-1]
 
     def isEmpty(self) -> bool:
-        return len(self.queue)==0
-        
+        return self.count==0
 
     def isFull(self) -> bool:
-        return len(self.queue)==self.k
-        
+        return self.count==self.size
 
 
 # Your MyCircularDeque object will be instantiated and called as such:
