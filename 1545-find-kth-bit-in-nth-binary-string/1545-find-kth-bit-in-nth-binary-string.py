46b3b918c55246acc35c7005a8f090efc9c22c32
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-03-11 23:16:42 +0300

    Time: 423 ms (30.49%), Space: 27.3 MB (11.8%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
@@ -2,13 +2,7 @@ class Solution:
     def solve(self,s,n):
         if n==1:
             return s
-        t = list(s)
-        for i in range(len(t)):
-            if t[i] == '0':
-                t[i] = '1'
-            else:
-                t[i] = '0'
-        t = ''.join(t[::-1])
+        t =  ''.join('1' if ch == '0' else '0' for ch in s)[::-1]
         s = s + '1' + t
         return self.solve(s,n-1)
 
