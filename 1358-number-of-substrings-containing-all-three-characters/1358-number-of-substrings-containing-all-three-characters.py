80311bc740e460106caf683633876a6025de306a
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-03-11 09:46:38 +0300

    Time: 231 ms (12.72%), Space: 18 MB (17.65%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
@@ -1,15 +1,20 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        hashh = {'a':-1,'b':-1,'c':-1}
-        max_substring = 0
+        def check(hashh):
+            see = ['a','b','c']
+            for val in see:
+                if val not in hashh or hashh[val]<1:
+                    return False
+            return True
 
-        for i,char in enumerate(s):
-            hashh[char] = i
+        hashh = {}
+        ans,n = 0,len(s)
+        j = 0
+        for i in range(n):
+            hashh[s[i]] = hashh.get(s[i],0)+1
+            while check(hashh):
+                ans+=(n-i)
+                hashh[s[j]]-=1
+                j+=1
 
-            if hashh['a']!=-1 and hashh['b']!=-1 and hashh['b']!=-1:
-                max_substring += min(hashh.values())+1
-
-        return max_substring
-
-
-        
+        return ans
