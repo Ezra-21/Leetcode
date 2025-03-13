86411646418cb98500230d9bcb05e57898ae61ff
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-03-13 21:53:35 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.py
+++ 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.py
@@ -2,24 +2,26 @@ class Solution:
     def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
         n = len(nums)
         prefix = [0]*(n+1)
-        summ ,j = 0,0
+        summ = 0
+        j = 0
 
-        for i in range(n):
-            while summ+prefix[i] < nums[i]:
-                if j==len(queries):
-                    return -1
-                s,e,val = queries[j]
-                j+=1
-                if e<i:
-                    continue
-                s = max(s,i)
-                prefix[s]+=val
-                prefix[e+1]-=val
+        for i,(s,e,val) in enumerate(queries):
+            while j<n:
+                if nums[j]<=summ+prefix[j]:
+                    summ+=prefix[j]
+                    j+=1
+                else:
+                    break
+                if j==n:
+                    return i
 
-            summ+=prefix[i]
+            if e<j:
+                continue
+            s = max(j,s)
+            prefix[s] += val
+            prefix[e+1] -= val
 
             
-        
-        return j
+        return -1
 
 
