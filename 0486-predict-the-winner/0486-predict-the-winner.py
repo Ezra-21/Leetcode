807a789dd74397b857df5dd0fa844b431ba6996f
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-03-14 09:35:48 +0300

    Time: 0 ms (100%), Space: 18.1 MB (30.62%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,6 +1,5 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        
         def solve(l,r):
             if dp[l][r]!=-1:
                 return dp[l][r]
@@ -16,7 +15,7 @@ class Solution:
         
         n = len(nums)
         dp = [[-1]*n for _ in range(n)]
-        return solve(0,len(nums)-1) >= 0
+        return solve(0,n-1) >= 0
 
 
 
