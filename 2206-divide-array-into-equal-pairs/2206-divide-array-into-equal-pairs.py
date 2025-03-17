afcaf77f39f687f88633fb70e66238c50d8938e8
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-03-17 08:55:03 +0300

    Time: 3 ms (70.86%), Space: 18 MB (44.86%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.py
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.py
@@ -1,7 +1,7 @@
 class Solution:
     def divideArray(self, nums: List[int]) -> bool:
         hashh = Counter(nums)
-        for _,fre in hashh.items():
+        for fre in hashh.values():
             if fre%2!=0:
                 return False
         return True
