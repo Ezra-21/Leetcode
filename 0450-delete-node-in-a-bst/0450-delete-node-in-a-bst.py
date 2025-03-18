7be785f4202a6ef0b6da45bc41d92d6adcec3a8e
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-03-18 15:23:30 +0300

    Time: 0 ms (100%), Space: 21.5 MB (31.04%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -5,30 +5,32 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def findmin(self,node):
-        while node.left:
-            node = node.left
-        return node
+    def find(self,head):
+        while head.left:
+            head = head.left
+        return head
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         if not root:
             return None
 
-        if key>root.val:
-            root.right = self.deleteNode(root.right,key)
-        elif key<root.val:
+        if root.val>key:
             root.left = self.deleteNode(root.left,key)
+        elif root.val<key:
+            root.right = self.deleteNode(root.right,key)
         else:
+
             if not root.left and not root.right:
                 return root.right
+
             if not root.left:
                 return root.right
-            elif not root.right:
+                
+            if not root.right:
                 return root.left
-            else:
-                temp = self.findmin(root.right)
-                root.val = temp.val
-                root.right = self.deleteNode(root.right,temp.val)
-            
 
-
+            newvalue = self.find(root.right)
+            root.val = newvalue.val
+            root.right = self.deleteNode(root.right,newvalue.val)
         return root
+
+        
