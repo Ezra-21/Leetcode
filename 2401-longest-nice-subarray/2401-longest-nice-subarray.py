9595c3483bcfa688d4a823d650c640692b2e3d0a
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-03-18 09:38:54 +0300

    Time: 89 ms (68.6%), Space: 33.6 MB (26.22%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.py
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.py
@@ -1,31 +1,13 @@
 class Solution:
-    def expand(self,check,binary):
-        for i,val in enumerate(binary):
-            val = int(val)
-            if check[i]&val:
-                return False
-        for i,val in enumerate(binary):
-            val = int(val)
-            check[i]+=val
-        return True
-
-    def shrink(self,check,binary):
-        for i,val in enumerate(binary):
-            val = int(val)
-            check[i]-=val
-                
-            
+    
     def longestNiceSubarray(self, nums: List[int]) -> int:
-        check = [0]*30
-        ans = 1
-        j = 0
-        for i,num in enumerate(nums):
-            binary = format(num,'030b')
-            while not self.expand(check,binary):
-                binn = format(nums[j],'030b')
-                self.shrink(check,binn)
-                j+=1
+        ans,check,j = 0,0,0
 
+        for i,num in enumerate(nums):
+            while check&num:
+                check^=nums[j]
+                j+=1
+            check|=num
             ans = max(ans,i-j+1)
 
         return ans
