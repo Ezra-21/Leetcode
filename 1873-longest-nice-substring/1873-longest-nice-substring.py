b25c3eac21e92972335a4c3034f800cf47b0b27f
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-03-20 19:46:23 +0300

    Time: 2 ms (81.15%), Space: 17.8 MB (38.31%) - LeetHub

---
+++ 1873-longest-nice-substring/1873-longest-nice-substring.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestNiceSubstring(self, s: str) -> str:
+        if len(s) < 2:
+            return ""
+
+        def solve(sub):
+            if len(sub) < 2:
+                return ""
+
+            unique = set(sub)
+            for i, char in enumerate(sub):
+                if char.swapcase() not in unique:
+                    left = solve(sub[:i])
+                    right = solve(sub[i + 1:])
+                    return left if len(left) >= len(right) else right
+
+            return sub 
+
+        return solve(s)
