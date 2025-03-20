a4a7c2bc683f9605e09083b450204339e4adcfcf
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-03-20 19:10:12 +0300

    Time: 37 ms (39.65%), Space: 17.3 MB (99.9%) - LeetHub

--- 1763-longest-nice-substring/1763-longest-nice-substring.py
+++ 1763-longest-nice-substring/1763-longest-nice-substring.py
@@ -2,15 +2,15 @@ class Solution:
     def longestNiceSubstring(self, s: str) -> str:
         def nice_substring(substring):
             for char in substring:
-                if char.lower() not in substring and char.upper() not in substring:
+                if char.lower() not in substring or char.upper() not in substring:
                     return False
             return True
 
 
         longest_subtring = ''
         for i in range(len(s)):
-            for j in range(i,len(s)+1):
-                substring = s[i:j]
+            for j in range(i,len(s)):
+                substring = s[i:j+1]
                 if nice_substring(substring) and len(substring)>len(longest_subtring):
                     longest_subtring = substring
         return longest_subtring
