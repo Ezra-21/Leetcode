511f204c4953a99b702ecbe1432e6b67a1f69d0f
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-04-06 10:23:24 +0300

    Time: 8 ms (66.27%), Space: 18.4 MB (89.87%) - LeetHub

--- 2643-row-with-maximum-ones/2643-row-with-maximum-ones.py
+++ 2643-row-with-maximum-ones/2643-row-with-maximum-ones.py
@@ -3,18 +3,6 @@ class Solution:
         ans = [0,0]
         for i,row in enumerate(mat):
             count = row.count(1)
-            if count>ans[0]:
+            if count>ans[1]:
                 ans = [i,count]
-        return ans
-
-class Solution:
-    def rowAndMaximumOnes(self, mat: List[List[int]]) -> List[int]:
-        ans = [0, 0]
-
-        for i, row in enumerate(mat):
-            ones_count = sum(row)
-
-            if ones_count > ans[1]:
-                ans = [i, ones_count]
-
         return ans
