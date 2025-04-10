5bfbcb0f87278aa3ab3a293fd569c0c46797c604
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-04-10 10:31:47 +0300

    Time: 378 ms (7.93%), Space: 22.1 MB (55.49%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -4,12 +4,14 @@ class Solution:
         ans = right
         while left<=right:
             weight = (left+right)//2
-            summ,count = 0,1
+            summ,count = 0,0
             for i,val in enumerate(weights):
                 summ+=val
                 if summ>weight:
                     count+=1
                     summ = val
+                if i==len(weights)-1:
+                    count+=1
                 
             if count <= days:
                 ans = weight
