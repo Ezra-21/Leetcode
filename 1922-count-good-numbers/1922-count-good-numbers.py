c82715b506ca3a1d6d0f689339e4b88ff7ad074c
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-04-25 13:27:28 +0300

    Time: 0 ms (100%), Space: 17.9 MB (43.1%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -3,4 +3,4 @@ class Solution:
         MOD = 10**9 + 7
         even_pos = math.ceil(n/2)
         odd_pos = n-even_pos
-        return (pow(5,even_pos)*pow(4,odd_pos))%MOD
+        return (pow(5,even_pos,MOD)*pow(4,odd_pos,MOD))%MOD
