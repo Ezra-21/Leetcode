80affd2005d6e959eb87c75ba09a9cc615959785
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-04-26 18:46:25 +0300

    Time: 396 ms (29.5%), Space: 20.4 MB (63.89%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -5,10 +5,15 @@ class Solution:
         for u,v in prerequisites:
             graph[u][v] = True
 
-        
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    if graph[i][k] and graph[k][j]:
+                        graph[i][j] = True
 
         ans = []
         for u,v in queries:
             ans.append(graph[u][v])
 
         return ans
+
