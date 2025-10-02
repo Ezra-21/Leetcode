abe88a1cbd5486bf7882a3fc9b8a240eaa826633
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-10-02 10:24:42 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,20 +1,29 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        s = s.lstrip()
-        sign = 1
-        ans = 0
-        i = 0
-        n = len(s)
-        
-        if i < n and (s[i] == '+' or s[i] == '-'):
-            if s[i] == '-':
-                sign = -1
-            i += 1
+        INT_MIN = -2**31
+        INT_MAX = 2**31
+        ans = ''
+        pos,check = True,True
+        for i,ch in enumerate(s):
+            if check and (ch=='-' or ch=='+'):
+                if ch=='-':
+                    pos = False
+                check = False
+            elif ch.isdigit() :
+                ans+=ch
+                check = False
+            elif (ch == ' ' and check):
+                continue
+            else:
+                break
+        if ans:
+            ans = int(ans)
+            ans = max(INT_MIN, min(ans, INT_MAX))
+        else:
+            ans = 0
+        return ans if pos else -ans
+           
+                
+
             
-        while i < n and s[i].isdigit():
-            ans = ans * 10 + int(s[i])
-            i += 1
-        
-        ans *= sign
-        ans = max(-(2**31), min((2**31)-1, ans))
-        return ans
+            
