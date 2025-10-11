4e537b27f888847dcbb7e3bf11eea089457de0e7
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-10-11 10:06:19 +0300

    Time: 99 ms (23.2%), Space: 17.9 MB (47.29%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,30 +1,68 @@
-class Linked:
-    def __init__(self, val):
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
         self.next = None
-        self.val = val
+
+
+class DoublyCircularLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_end(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            new_node.next = new_node
+            new_node.prev = new_node
+            return
+
+        last = self.head.prev  
+
+        new_node.next = self.head
+        new_node.prev = last
+        last.next = new_node
+        self.head.prev = new_node
+
+    def delete(self, key):
+        if self.head.data == key:
+            last = self.head.prev
+            self.head = self.head.next
+            self.head.prev = last
+            last.next = self.head
+            return
+
+        # Delete middle or end by value
+        current = self.head.next
+        while current != self.head:
+            if current.data == key:
+                prev_node = current.prev
+                next_node = current.next
+                prev_node.next = next_node
+                next_node.prev = prev_node
+                return
+            current = current.next
+
+    def check(self):
+        return self.head.next == self.head
 
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        dummy = Linked(1)
-        curr = dummy
-        for i in range(2, n + 1):
-            node = Linked(i)
-            curr.next = node
-            curr = node
-        curr.next = dummy  
-
-        curr = dummy  
-
-        temp = curr
+        arr = DoublyCircularLinkedList()
+        for i in range(n):
+            arr.insert_end(i+1)
         
+        current = arr.head
+        while not arr.check():
+            for _ in range(k-1):
+                current = current.next
+            temp = current.data
+            current = current.next
+            arr.delete(temp)
 
-        while n > 1:
-            for _ in range(k - 1):
-                temp = curr
-                curr = curr.next
+        return current.data
 
-            temp.next = curr.next  
-            curr = temp.next  
-            n -= 1  
 
-        return curr.val  
+                
+        
