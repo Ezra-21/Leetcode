948859c379ad9c03b08d1066777e3ed494184667
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-10-12 11:47:40 +0300

    Time: 3 ms (53.43%), Space: 17.6 MB (98.81%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,68 +1,9 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.prev = None
-        self.next = None
-
-
-class DoublyCircularLinkedList:
-    def __init__(self):
-        self.head = None
-
-    def insert_end(self, data):
-        new_node = Node(data)
-
-        if self.head is None:
-            self.head = new_node
-            new_node.next = new_node
-            new_node.prev = new_node
-            return
-
-        last = self.head.prev  
-
-        new_node.next = self.head
-        new_node.prev = last
-        last.next = new_node
-        self.head.prev = new_node
-
-    def delete(self, key):
-        if self.head.data == key:
-            last = self.head.prev
-            self.head = self.head.next
-            self.head.prev = last
-            last.next = self.head
-            return
-
-        # Delete middle or end by value
-        current = self.head.next
-        while current != self.head:
-            if current.data == key:
-                prev_node = current.prev
-                next_node = current.next
-                prev_node.next = next_node
-                next_node.prev = prev_node
-                return
-            current = current.next
-
-    def check(self):
-        return self.head.next == self.head
-
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        arr = DoublyCircularLinkedList()
-        for i in range(n):
-            arr.insert_end(i+1)
+        def helper(n,k):
+            if n==1:
+                return 0
+
+            return (helper(n-1,k) + k) % n
         
-        current = arr.head
-        while not arr.check():
-            for _ in range(k-1):
-                current = current.next
-            temp = current.data
-            current = current.next
-            arr.delete(temp)
-
-        return current.data
-
-
-                
-        
+        return helper(n,k)+1
