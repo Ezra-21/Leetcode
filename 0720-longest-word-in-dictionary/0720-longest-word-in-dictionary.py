bb23a52a866b02156bc20869dddb5e21347bd8e5
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-10-15 19:19:36 +0300

    Time: 6 ms (86.84%), Space: 18.3 MB (59.57%) - LeetHub

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
@@ -1,14 +1,15 @@
+from typing import List
+
 class Solution:
     def longestWord(self, words: List[str]) -> str:
-        words.sort()
-        sett = set()
-
+        words.sort()  # Sort lexicographically
+        sett = set([''])  # Add empty string to handle single-character words
         res = ''
+        
         for word in words:
-            if word[:-1] in sett:
-                if len(res) < len(word):
+            if word[:-1] in sett:  # Check if the prefix exists
+                sett.add(word)
+                if len(word) > len(res):
                     res = word
-
-            sett.add(word)
-
+        
         return res
