333d2b9a3dbaba144bfa65e20bcb7e6588a6b023
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-10-24 18:18:31 +0300

    Time: 20 ms (81.75%), Space: 18.2 MB (87.83%) - LeetHub

---
+++ 0676-implement-magic-dictionary/0676-implement-magic-dictionary.py
@@ -0,0 +1,24 @@
+class MagicDictionary:
+
+    def __init__(self):
+        self.words = []
+
+    def buildDict(self, dictionary: list[str]) -> None:
+        self.words = dictionary
+
+    def search(self, searchWord: str) -> bool:
+        for word in self.words:
+            if len(word) != len(searchWord):
+                continue
+
+            diff = 0
+            for a, b in zip(word, searchWord):
+                if a != b:
+                    diff += 1
+                if diff > 1:
+                    break
+
+            if diff == 1:
+                return True
+
+        return False
