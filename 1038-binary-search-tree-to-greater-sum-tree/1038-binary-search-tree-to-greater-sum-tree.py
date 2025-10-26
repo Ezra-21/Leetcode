8a9acd382682918a675ea86711db6d62a168a7a3
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-10-26 10:44:38 +0300

    Time: 0 ms (100%), Space: 17.8 MB (36.37%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.py
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.py
@@ -2,23 +2,19 @@
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
-#         self.left = left
+#         self.left = left 
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.sum = 0
-    def dfs(self,node):
-        if not node:
-            return
-        self.dfs(node.right)
-        self.sum+=node.val
-        node.val = self.sum
-        self.dfs(node.left)
-        return self.sum
-
-
     def bstToGst(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        self.dfs(root)
+        summ = 0
+        def dfs(root):
+            nonlocal summ
+            if not root:
+                return
+            dfs(root.right)
+            summ += root.val
+            root.val = summ
+            dfs(root.left)
+        dfs(root)
         return root
-
         
