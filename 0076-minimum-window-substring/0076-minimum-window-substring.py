28285d7691443003db31b88be7242faba2b1c813
Ezra <168126853+Ezra-21@users.noreply.github.com>
2025-11-11 17:49:38 +0300

    Time: 103 ms (39.21%), Space: 18.3 MB (53.3%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,29 +1,30 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        def check(hashh1,hashh2):
-            for val,fre in hashh2.items():
-                if hashh1[val]<fre:
-                    return False
+        n, m = len(s), len(t)
 
-            return True
-
-        n,m = len(s),len(t)
-
-        if m>n:
+        if m > n:
             return ''
         hashh_t = Counter(t)
-        hashh_include = Counter()
-        length,index = float('inf'),[]
-        j = 0
-        for i in range(n):
-            hashh_include[s[i]]+=1
-            while (i-j+1)>=len(t) and check(hashh_include,hashh_t):
-                if length>(i-j+1):
-                    length = (i-j+1)
-                    index = [j,i]
-                hashh_include[s[j]]-=1
-                j+=1
-
-        return s[index[0]:index[1]+1] if index else ''
-            
+        hashh_s = Counter()
         
+        length, index = float('inf'), []
+        check= 0
+        l = 0
+        
+        
+        for i in range(n):   
+            hashh_s[s[i]] +=1
+            if s[i] in hashh_t and hashh_s[s[i]] == hashh_t[s[i]]:
+                check += 1
+            
+            while check == len(hashh_t):
+                if length > i-l + 1:
+                    length = i - l + 1
+                    index = [l, i]
+
+                hashh_s[s[l]]-=1
+                if s[l] in hashh_t and hashh_s[s[l]]<hashh_t[s[l]]:
+                    check -= 1  
+                l += 1
+
+        return s[index[0]:index[1] + 1] if index else ''
